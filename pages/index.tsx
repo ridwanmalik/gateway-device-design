a50bb07722552a441ab43033a4b2ff66db57641c
ridwanmalik <skredonemalik@gmail.com>
2022-06-22 20:28:48 +0600

    Gateway design

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,81 +4,61 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-start justify-start py-5">
       <Head>
-        <title>Create Next App</title>
+        <title>Example</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="w-full h-full">
+        <div className="container mx-auto">
+          <h1 className="title text-xl font-medium mb-3">Geberit devices and gateways</h1>
+          <div className="gateways space-y-4">
+            <p className="company-name text-sm font-bold">Tammisaari</p>
+            <div className="gateway-wrapper ml-4 flex items-center space-x-3 bg-gray-100 rounded">
+              <a href="#" className="gateway-card block p-6 min-w-[15rem] max-w-sm bg-white rounded border shadow-md hover:bg-gray-100 border-l-8 border-green-700">
+                <h5 className="text-xl font-bold tracking-tight text-gray-900 mb-3">Gateway 1</h5>
+                <p className="text-sm font-normal text-gray-700">status: online</p>
+                <p className="text-sm font-normal text-gray-700">working properly</p>
+              </a>
+              <a href="#" className="device-card block p-4 min-w-[15rem] max-w-[20rem] bg-white rounded border shadow-md hover:bg-gray-100 border-l-8 border-green-700">
+                <h5 className="text-xl font-bold tracking-tight text-gray-900 mb-3">Lösare 1</h5>
+                <p className="text-sm font-normal text-gray-700">Viscosity: 54 mPas​</p>
+                <p className="text-sm font-normal text-gray-700">Device status: ok​</p>
+              </a>
+              <a href="#" className="device-card block p-4 min-w-[15rem] max-w-[20rem] bg-white rounded border shadow-md hover:bg-gray-100 border-l-8 border-red-600">
+                <h5 className="text-xl font-bold tracking-tight text-gray-900 mb-3">Blandare 2​</h5>
+                <p className="text-sm font-normal text-gray-700">Offline</p>
+                <p className="text-sm font-normal text-gray-700">Last seen 20.05.2022 12:12​</p>
+              </a>
+              <a href="#" className="device-card block p-4 min-w-[15rem] max-w-[20rem] bg-white rounded border shadow-md hover:bg-gray-100 border-l-8 border-yellow-400">
+                <h5 className="text-xl font-bold tracking-tight text-gray-900">FK3C</h5>
+                <p className="text-sm font-normal text-gray-700">Viscosity 60 mPas</p>
+                <p className="text-xs font-normal text-yellow-500">Viscosity exeeded the threshold value of 58 mPas​e</p>
+                <p className="text-sm font-normal text-gray-700">Device status: ok​</p>
+              </a>
+              <a href="#" className="device-card block p-4 min-w-[15rem] max-w-[20rem] bg-white rounded border shadow-md hover:bg-gray-100 border-l-8 border-red-600">
+                <h5 className="text-xl font-bold tracking-tight text-gray-900 mb-3">Blandare 3</h5>
+                <p className="text-sm font-normal text-gray-700">status: online</p>
+                <p className="text-sm font-normal text-gray-700">working properly</p>
+              </a>
+            </div>
+            <p className="company-name text-sm font-bold">Haldensleben</p>
+            <div className="gateway-wrapper ml-4 flex items-center space-x-3 bg-gray-100 rounded">
+              <a href="#" className="gateway-card block p-6 min-w-[15rem] max-w-sm bg-white rounded border shadow-md hover:bg-gray-100 border-l-8 border-red-600">
+                <h5 className="text-xl font-bold tracking-tight text-gray-900 mb-3">Gateway 2</h5>
+                <p className="text-sm font-normal text-gray-700">Offline</p>
+                <p className="text-sm font-normal text-gray-700">Last seen 20.05.2022 12:12​</p>
+              </a>
+              <a href="#" className="device-card block p-4 min-w-[15rem] max-w-[20rem] bg-white rounded border shadow-md hover:bg-gray-100 border-l-8 border-red-600">
+                <h5 className="text-xl font-bold tracking-tight text-gray-900 mb-3">Haldensleben Lab​​</h5>
+                <p className="text-sm font-normal text-gray-700">Offline</p>
+                <p className="text-sm font-normal text-gray-700">Last seen 20.05.2022 12:12​</p>
+              </a>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
